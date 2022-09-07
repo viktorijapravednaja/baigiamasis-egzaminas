64497166af8e6e1c91dd0b453934d4c8b292c757
Viktorija Pravednaja <v.praved@gmail.com>
2022-09-07 18:42:11 +0300

    changes

--- 2_backend/app.js
+++ 2_backend/app.js
@@ -1,19 +1,41 @@
 const express = require("express");
+const cors = require("cors");
+
 require("dotenv").config();
 
 const app = express();
 PORT = process.env.PORT || 5000;
 
 const connectMongoDB = require("./config/db.js");
+const Customer = require("./models/Customer.model.js");
 
 // -- connecting DB
 connectMongoDB();
 
 //Middlewares
 app.use(express.json());
+app.use(cors());
+
+//Routes
+app.get("/", (req, res) => {
+  res.send("SUP");
+});
+
+app.post("/registration", async (req, res) => {
+  const customer = new CustomerSchema({
+    name: "Jogn",
+    email: "j@g.com",
+    date: "20*=-2-2",
+    time: "14:25",
+  });
+  try {
+    await customer.save();
+    res.send("inserted data");
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port:${PORT}`);
 });
-
-//Routes
---
+++ 2_backend/models/Customer.model.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const CustomerSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: String,
+    required: true,
+  },
+  time: {
+    type: String,
+    required: true,
+  },
+});
+
+const Customer = mongoose.model("Customer", CustomerSchema);
+module.exports = Customer;
